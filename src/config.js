c44ab742f1f82ebc51c67a139236ee6b952a1d91
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-03 21:36:37 -0300

    Funciona con base de datos de railway en la nube

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import { config } from "dotenv";
+config();
+
+export const PORT = process.env.PORT || 6170;
+export const DB_HOST = "containers-us-west-127.railway.app";
+export const DB_USER = "root";
+export const DB_PASSWORD = "V7ewl7LE6sceR1wlMgLL";
+export const DB_DATABASE = "railway";
+export const DB_PORT = 5563;
