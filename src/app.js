49935308d8ee3c6376b2bc5274ca4719925a53b2
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-07 21:11:07 -0300

    Se separe el codigo del algoritmo excel en otro archivo aparte

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 import productsRoutes from "./routes/products.routes.js";
+import { processExcelData } from "./quimicoExcelSQL.js";
 
 
 
@@ -44,95 +45,8 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
 });
 
-
-
-
-// Resto de la configuración de tu servidor...
-// Resto de la configuración de tu servidor....
-
-// Configura la conexión a la base de datos MySQL
-const dbConfig = {
-  host: 'containers-us-west-127.railway.app',
-  user: 'root',
-  password: 'V7ewl7LE6sceR1wlMgLL',
-  port: 5563,
-  database: 'railway',
-  connectTimeout: 60000, // Aumenta el tiempo de espera a 60 segundos (o más si es necesario)
-
-};
-
-
-// Función para insertar o actualizar datos en la base de datos
-async function insertOrUpdateDatos(quimicoId, code, description, presentation, dealerPrice, retailPrice, costoKilo) {
-  try {
-    const connection = await mysql2.createConnection(dbConfig);
-
-    // Consulta si 'code' ya existe en la base de datos
-    const [rows] = await connection.execute(
-      'SELECT * FROM quimicoNormal WHERE code = ?',
-      [code]
-    );
-
-    if (rows.length > 0) {
-      // Si 'code' existe, actualiza la fila existente
-      await connection.execute(
-        'UPDATE quimicoNormal SET dealerPrice = ?, retailPrice = ?, costoKilo = ?, description = ?, presentation = ? WHERE code = ?',
-        [dealerPrice, retailPrice, costoKilo, description, presentation, code]
-      );
-
-      console.log(`Registro actualizado para el código ${code}`);
-    } else {
-      // Si 'code' no existe, inserta una nueva fila
-      await connection.execute(
-        'INSERT INTO quimicoNormal (quimicoId, code, description, presentation, dealerPrice, retailPrice, costoKilo) ' +
-        'VALUES (?, ?, ?, ?, ?, ?, ?)',
-        [quimicoId, code, description, presentation, dealerPrice, retailPrice, costoKilo]
-      );
-
-      console.log(`Nuevo registro insertado para el código ${code}`);
-    }
-
-    // Cierra la conexión a la base de datos
-    await connection.end();
-  } catch (error) {
-    console.error('Error al insertar o actualizar datos en la base de datos:', error);
-    console.log(code);
-  }
-}
-
-// Lee el archivo Excel
-const workbook = xlsx.readFile('./src/archivo.xlsm');
-
-// Selecciona la hoja 'Quimicos'
-const worksheet = workbook.Sheets['Quimicos'];
-
-// Recorre las filas desde A7 hasta A100
-for (let rowNum = 7; rowNum <= 100; rowNum++) {
-  const quimicoId = null; // Valor fijo para quimicoId
-  let code = worksheet[`A${rowNum}`] ? worksheet[`A${rowNum}`].v : null;
-  const description = worksheet[`B${rowNum}`] ? worksheet[`B${rowNum}`].v : null;
-  const presentation = worksheet[`C${rowNum}`] ? worksheet[`C${rowNum}`].v : null;
-  const dealerPrice = worksheet[`D${rowNum}`] ? parseFloat(worksheet[`D${rowNum}`].v).toFixed(2) : null;
-  const retailPrice = worksheet[`E${rowNum}`] ? parseFloat(worksheet[`E${rowNum}`].v).toFixed(2) : null;
-  const costoKilo = worksheet[`F${rowNum}`] ? parseFloat(worksheet[`F${rowNum}`].v).toFixed(2) : null;
-
-// Convertir 'code' en una cadena (varchar) si no lo es
-if (code !== null && typeof code !== 'string') {
-  code = code.toString();
-}
-
-// Si 'code' es "Codigo" o no es una cadena vacía, ignora esta fila y no la insertes en la base de datos
-if (typeof code === 'string' && code.trim() !== "" && code !== "Codigo") {
-  // Inserta o actualiza los datos en la base de datos
-  insertOrUpdateDatos(quimicoId, code, description, presentation, dealerPrice, retailPrice, costoKilo);
-  console.log(quimicoId, code, description, presentation, dealerPrice, retailPrice, costoKilo);
-} else {
-  console.log(`Fila con 'code' igual a "Codigo" o vacío ignorada`);
-  console.log(`Fila ${rowNum} - Código: "${code}"`);
-}
-};
-// 
-// Resto del código de tu aplicación.....
+// Procesa los datos del archivo Excel
+processExcelData('./src/archivo.xlsm');
 
 
 
