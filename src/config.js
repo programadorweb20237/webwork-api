ba91624b3df173e66f75e4e9741f8d0ca4406140
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-10 22:26:58 -0300

    test74564564

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 6170;
-export const DB_HOST = "45.77.158.192";
+export const DB_HOST = "servidor5.websail.com.ar";
 export const DB_USER = "dairycom_nuevvo";
 export const DB_PASSWORD = "buenosaires159951";
 export const DB_DATABASE = "dairycom_productNormal";
