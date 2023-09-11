52e7f7192b9af4b6bad0ab7079617d2c12a39d94
unknown <testprogramacion2023@outlook.com>
2023-09-11 12:38:08 -0300

    test01

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,6 +5,6 @@ export const getProducts = async (req, res) => {
     const [rows] = await pool.query("SELECT * FROM productNormal");
     res.json(rows);
   } catch (error) {
-    return res.status(500).json({ message: "Something goes wrong (ProductNormal)" });
+    return res.status(500).json({ message: "Something goes wrong (ProductNormalll)" });
   }
 };
