cc1ba5391ab3d08d4268a0d4dc931ddc5af271b8
unknown <testprogramacion2023@outlook.com>
2023-09-11 12:29:27 -0300

    FUNCIONA LA CONEXION A DB

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ config();
 
 export const PORT = process.env.PORT || 6170;
 export const DB_HOST = "45.77.158.192";
+//si no funciona, quitarle _nuevvo a USER.
 export const DB_USER = "dairycom_nuevvo";
 export const DB_PASSWORD = "buenosaires159951";
 export const DB_DATABASE = "dairycom_productNormal";
