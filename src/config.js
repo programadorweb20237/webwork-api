082c5cbacffe2946cd652343ec35044d4b81a34f
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-10 21:14:00 -0300

    test4645640

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 
 export const PORT = process.env.PORT || 6170;
 export const DB_HOST = "45.77.158.192";
-export const DB_USER = "dairycom";
-export const DB_PASSWORD = "buenosaires195591";
-export const DB_DATABASE = "productNormal";
+export const DB_USER = "dairycom_nuevvo";
+export const DB_PASSWORD = "buenosaires159951";
+export const DB_DATABASE = "dairycom_productNormal";
 export const DB_PORT = 3306;
